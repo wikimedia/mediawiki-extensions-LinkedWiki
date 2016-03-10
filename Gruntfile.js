a73363a50dc590ee023741acc337668097d0642f
Paladox <thomasmulhall410@yahoo.com>
2016-03-10 22:57:41 +0000

    Add npm entry point
    Change-Id: Iff002634c0e30670ae8f49817f38760a77df922b

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	// grunt.loadNpmTasks( 'grunt-banana-checker' );
+
+	grunt.initConfig( {
+		/* banana: {
+			linkedwiki: 'i18n/',
+			sparqlflinteditor: 'i18n/',
+			sparqlquery: 'i18n/'
+		}, */
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jsonlint' /* 'banana' */ ] );
+	grunt.registerTask( 'default', 'test' );
+};
