d90fb51d39c4e86c57d0cd28e6c4b96cd45b157b
Umherirrender <umherirrender_de.wp@web.de>
2017-10-22 12:53:20 +0200

    Add missing qqq message documentation
    Activate banana checker
    
    Change-Id: I05aef1677f56f640d6e4ab239e7b519edc3e5795

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,26 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
-	// grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
 
 	grunt.initConfig( {
-		/* banana: {
-			linkedwiki: 'i18n/',
-			linkedwikiconfig: 'i18n/',
-			sparqlflinteditor: 'i18n/',
-			sparqlquery: 'i18n/'
-		}, */
+		banana: {
+			linkedwiki: 'i18n/linkedwiki/',
+			linkedwikiconfig: 'i18n/linkedwikiconfig/',
+			rdfsave: 'i18n/rdfsave/',
+			rdfunit: 'i18n/rdfunit/',
+			sparqlflinteditor: 'i18n/sparqlflinteditor/',
+			sparqlquery: 'i18n/sparqlquery/'
+		},
 		jsonlint: {
 			all: [
 				'**/*.json',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint' /* 'banana' */ ] );
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
